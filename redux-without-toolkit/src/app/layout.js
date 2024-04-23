1bfa1fcc4648158912257c365026a69f4c6dab2a
Luis Fernando <luis.silva9902@gmail.com>
2024-04-23 00:00:06 -0300

    alteracoes no store do redux

--- redux-without-toolkit/src/app/layout.js
+++ redux-without-toolkit/src/app/layout.js
@@ -1,21 +1,24 @@
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
-import { Providers } from "@/providers";
+import { StoreInitializer } from "@/store";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Testing redux",
-  description: "testing redux without toolkit",
+  title: "Redux Without Toolkit",
+  description: "Testing redux without toolkit",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <Providers>
+    <StoreInitializer>
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
-    </Providers>
-    
+    </StoreInitializer>
   );
 }
--- redux-without-toolkit/src/store.js
+++ redux-without-toolkit/src/store.js
@@ -1,9 +1,57 @@
-import { createStore, combineReducers } from 'redux'
-import { taskList, taskHistory } from './reducers'
+"use client";
+import { combineReducers, createStore } from "redux";
+import { taskList, taskHistory } from "./reducers";
+import { useEffect, useState } from "react";
+import { Provider } from "react-redux";
+
+function loadState() {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch {
+    return undefined;
+  }
+}
+
+function saveState(state) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 const reducer = combineReducers({
   tasks: taskList,
-  history: taskHistory
-})
+  history: taskHistory,
+});
 
-export const store = createStore(reducer)
+function StoreInitializer({ children }) {
+  const [preloadedState, setPreloadedState] = useState(undefined);
+
+  useEffect(() => {
+    const initialState = loadState();
+
+    setPreloadedState(initialState);
+  }, []);
+
+  const store = createStore(reducer, preloadedState);
+
+  const unsubscribe = store.subscribe(() => {
+    saveState(store.getState());
+  });
+
+  useEffect(() => {
+    return () => {
+      unsubscribe();
+    };
+  }, [unsubscribe]);
+
+  return <Provider store={store}>{children}</Provider>;
+}
+
+export { StoreInitializer };
